ea8c949fa267fac3912536555d5c673ac3c42f56
Cathy <Infayond@parentss-mbp.gateway.sonic.net>
2016-02-29 18:44:51 -0800

    Fighter class

--- Fighter.java
+++ Fighter.java
@@ -11,22 +11,24 @@ public class Fighter{
 	String name;
 	int level;
 
-
+	Type[] type  = new Type[2];
 	Move[] moves = new Move[4];
 
 	public Fighter(
 		String name,
+		Type[] type;
 		Move[] moves,
-		int level 
+		int level,
 		double hp,
 		double attack,
 		double defense,
 		double specialAttack,
-		double specialDefense
+		double specialDefense,
 		double speed) {
 		
 		this.name = name;
 		this.moves = moves;
+		this.type = type;
 		this.level = level;
 		this.hp = hp;
 		this.attack = attack;
@@ -34,9 +36,10 @@ public class Fighter{
 		this.specialAttack = specialAttack;
 		this.speed = speed;
 
-	};
+	    };
 	public static void main(String[] args) {
+		Type[] t = new Type[2];
 		Move[] m = new Move[4];
-		Fighter f = new Fighter(pillow, m, 2, 77.0,4.0, 85.0,32.0,88.0,55.0)
+		Fighter f = new Fighter(pillow, t, m, 2, 77.0,4.0, 85.0,32.0,88.0,55.0)
 	}
 }
--- Moves.java
+++ Moves.java
@@ -1,3 +1,26 @@
-Moves.java
-
 package CathyPokemon;
+
+	public class Move{
+		int pp;
+		String type;
+		String moveType;
+		
+		Effect[] effect = new Effect[]; 
+
+		public Move(
+			int pp,
+			String type,
+			String moveType,
+			Effect[] effect) {
+
+			this.pp = pp;
+			this.type = type;
+			this.moveType = moveType;
+			this.effect = effect;
+
+		    };
+		public static void main (String[] args) {
+			Effect[] e = new Effect["Pokemon faints"];
+			Move m = new Move(10,bug,status,e)
+		}
+	}
